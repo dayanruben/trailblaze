e8528da8db2afd3113ea5dbde8aef07f61846bb4
Brian Gardner <me@briangardner.tech>
2025-07-17 17:39:26 -0400

    Apply publish plugin at the top level (#31)

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,13 +9,13 @@ plugins {
   alias(libs.plugins.android.library) apply false
   alias(libs.plugins.kotlin.android) apply false
   alias(libs.plugins.kotlin.jvm) apply false
-  alias(libs.plugins.vanniktech.maven.publish) apply false
   alias(libs.plugins.dokka) apply false
   alias(libs.plugins.spotless) apply false
   alias(libs.plugins.dagp) apply false
   alias(libs.plugins.compose.compiler) apply false
   alias(libs.plugins.jetbrains.compose.multiplatform) apply false
   alias(libs.plugins.kotlin.multiplatform) apply false
+  alias(libs.plugins.vanniktech.maven.publish)
 }
 
 subprojects {
